972fe1e0f08ee6d2f4ab653875e11ba9e46b565d
MiguelPortes <miguelgompor167@gmail.com>
2025-07-01 20:28:14 -0600

    [ADD] se agrego lo chido

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import ReactDOM from 'react-dom/client';
+import CardsApp from './components/Cards'
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <h1 className="mt-2 text-center alert alert-success text-lg-center" >
+            Healthy Center
+          </h1>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>  
+    <CardsApp/>
     </>
   )
 }
